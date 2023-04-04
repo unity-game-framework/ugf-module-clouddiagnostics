877c0b4040e45ed1ec132449b44c6549d785a1df
Vorobey <vorobyovvorobyov@gmail.com>
2023-04-04 18:12:50 +0200

    Add CloudDiagnosticsUnityModule

---
+++ Packages/UGF.Module.CloudDiagnostics/Runtime/Unity/CloudDiagnosticsUnityModuleDescription.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UGF.Application.Runtime;
+
+namespace UGF.Module.CloudDiagnostics.Runtime.Unity
+{
+    public class CloudDiagnosticsUnityModuleDescription : ApplicationModuleDescription
+    {
+        public bool EnableCaptureExceptions { get; }
+        public uint LogBufferSize { get; }
+        public IReadOnlyDictionary<string, string> Metadata { get; }
+
+        public CloudDiagnosticsUnityModuleDescription(Type registerType, bool enableCaptureExceptions, uint logBufferSize, IReadOnlyDictionary<string, string> metadata)
+        {
+            RegisterType = registerType ?? throw new ArgumentNullException(nameof(registerType));
+            EnableCaptureExceptions = enableCaptureExceptions;
+            LogBufferSize = logBufferSize;
+            Metadata = metadata;
+        }
+    }
+}
